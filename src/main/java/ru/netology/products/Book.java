c4ee8a79cf712084cbd7ca460659318eb6608493
Дина Абрамова <nightfox87@mail.ru>
2022-06-22 12:01:48 +0300

    Initial commit

---
+++ src/main/java/ru/netology/products/Book.java
@@ -0,0 +1,20 @@
+package ru.netology.products;
+
+public class Book extends Product {
+
+    protected String author;
+
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Book(int id, String name, int price, String author) {
+        super(id, name, price);
+        this.author = author;
+    }
+}
---
+++ src/main/java/ru/netology/products/Product.java
@@ -0,0 +1,20 @@
+package ru.netology.products;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class Product {
+
+    protected int id;
+    protected String name;
+    protected int price;
+
+
+
+
+}
